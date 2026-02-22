edf4f5f58f9a055f3ac87b19b8a519e260a21933
nikitakelwada <nikita.kelwada.01@gmail.com>
2026-02-21 21:02:44 -0600

    all current changes

--- scenes/Player/player.gd
+++
@@ -1,7 +0,0 @@
-extends CharacterBody2D
-
-@export var speed: float = 400.0
-
-func _physics_process(_delta: float) -> void:
-	if Input.is_action_pressed("left") or Input.is_action_pressed("right") or Input.is_action_pressed("up") or Input.is_action_pressed("down"):
-		print("INPUT DETECTED")
